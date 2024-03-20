25212c9328b7f407021338fe51da24afd12b16c0
MD. Kamruzzaman <sohel.eightb@gmail.com>
2024-03-20 20:01:43 +0600

    font import done

--- src/Components/Headar.jsx
+++ src/Components/Headar.jsx
@@ -2,7 +2,7 @@
 const Headar = () => {
     return (
         <div>
-            <h1 className='text-6xl bg-red-600'>Kmowledge Cafe</h1>
+            <h1 className='text-6xl bg-red-600'>Knowledge Cafe</h1>
         </div>
     );
 };
